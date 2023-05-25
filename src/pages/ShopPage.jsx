ff8259ed56f862b9148223b210280020b7c87aa6
NEWUSER <chasevriend84@gmail.com>
2023-05-25 11:12:42 -0700

    new commit

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../components/Banner'
 import Footer from '../components/Footer'
 import Nav from '../components/Nav'
+import Products from '../components/Products'
 // import Shop from '../components/Shop'
 // import Product from '../components/Product'
 
@@ -10,7 +11,7 @@ const ShopPage = () => {
     <>
         <Nav />
         {/* <Shop /> */}
-        
+        <Products />
         <Banner />
         <Footer />
 
